d4e6d504aa6b8187d1b4febf478ee13f314bc633
Shatulova <shatulova@selectel.ru>
2017-11-20 11:21:05 +0300

    home work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
-import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import { addListener, removeListener, isAuthorized } from './AuthorizeApi';
+import Private from './Private';
+import Public from './Public';
+import Home from './Home';
+import Auth from './Auth';
 
 class App extends Component {
   state = {
-    isAuthorized
+    isAuthorized,
   };
 
   componentDidMount() {
@@ -16,11 +21,35 @@ class App extends Component {
   }
 
   handleAuthorize = isAuthorized => {
-    this.setState({isAuthorized});
+    this.setState({ isAuthorized });
   };
 
   render() {
-    return null;
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/auth">Войти</Link>
+          </li>
+          <li>
+            <Link to="/private">Секретная страница</Link>
+          </li>
+          <li>
+            <Link to="/public">Публичная страница</Link>
+          </li>
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+          {this.state.isAuthorized ? <Route path="/private" component={Private} /> : <Redirect from="/private" to="/auth" />}
+          <Route path="/public" component={Public} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </nav>
+    );
   }
 }
 
--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import * as AuthAPI from './AuthorizeApi';
+import { Redirect } from 'react-router-dom';
 
 class Auth extends Component {
+  state = {
+    email: '',
+    password: '',
+    isAutorisationError: false,
+    isAuthorized: false,
+  };
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    const isAuthorized = AuthAPI.authorizeUser(email, password);
+    this.setState({ isAutorisationError: !isAuthorized, isAuthorized });
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
   render() {
-    return null;
+    return (
+      <div>
+        {this.state.isAuthorized ? <Redirect to="/" /> : null}
+        <input
+          name="email"
+          placeholder="enter email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <br/> <br/>
+        <input
+          name="password"
+          placeholder="enter password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <br/> <br/>
+        {this.state.isAutorisationError ?  <p className="error">Invalid password or login</p> : null}
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
   }
 }
 
